d576e2c1e992f8b01a458118f16c728cd87eb80d
Darren-34810 <darren.denisson@student.umn.ac.id>
2022-01-04 23:29:12 +0700

    Update manage_book.blade.php

--- resources/views/manage_book.blade.php
+++ resources/views/manage_book.blade.php
@@ -5,67 +5,109 @@
     <div class="container my-5">
         <div class="row justify-content-center">
             <div class="col-md-11">
-    <form>
-        <div class="card">
-            <div class="card-body">
-        <h3> Insert Book Form</h3>
-        <div class="row mb-3">
-            <label class="col-sm-6 col-form-label">Nama</label>
-            <div class="col-sm-6">
-                <input type="text" class="form-control" >
-            </div>
-        </div>
-        <div class="row mb-3">
-            <label class="col-sm-6 col-form-label">Author</label>
-            <div class="col-sm-6">
-                <input type="text" class="form-control" >
-            </div>
-        </div>
-        <div class="row mb-3">
-            <label class="col-sm-6 col-form-label">Synopsis</label>
-            <div class="col-sm-6">
-                <textarea type="text" class="form-control" ></textarea>
-            </div>
-        </div>
-        <div class="row mb-3">
-            <label class="col-sm-6 col-form-label">Genre(s)</label>
-            <div class="col-sm-6">
-                <div class="form-check">
-                    <input class="form-check-input" type="checkbox" >
-                    <label class="form-check-label" for="gridCheck1">
-                        Example checkbox
-                    </label>
-                </div>
-                <div class="form-check">
-                    <input class="form-check-input" type="checkbox" >
-                    <label class="form-check-label" for="gridCheck1">
-                        Example checkbox
-                    </label>
-                </div>
-                <div class="form-check">
-                    <input class="form-check-input" type="checkbox" >
-                    <label class="form-check-label" for="gridCheck1">
-                        Example checkbox
-                    </label>
-                </div>
-            </div>
-        </div>
-            <div class="row mb-3">
-                <label class="col-sm-6 col-form-label">Price</label>
-                <div class="col-sm-6">
-                    <input type="text" class="form-control" >
-                </div>
-            </div>
-            <div class="input-group mb-3">
-                <label class="col-sm-6 ">Cover</label>
-                <div class="col-sm-6">
-                <input type="file" class="form-control" >
-                </div>
-            </div>
-                <button type="submit" class="btn btn-primary">Sign in</button>
-            </div>
-        </div>
-    </form>
+                <form>
+                    <div class="card">
+                        <div class="card-body">
+                            <h3> Insert Book Form</h3>
+                            <div class="row mb-3">
+                                <label class="col-sm-6 col-form-label">Nama</label>
+                                <div class="col-sm-6">
+                                    <input type="text" class="form-control">
+                                </div>
+                            </div>
+                            <div class="row mb-3">
+                                <label class="col-sm-6 col-form-label">Author</label>
+                                <div class="col-sm-6">
+                                    <input type="text" class="form-control">
+                                </div>
+                            </div>
+                            <div class="row mb-3">
+                                <label class="col-sm-6 col-form-label">Synopsis</label>
+                                <div class="col-sm-6">
+                                    <textarea type="text" class="form-control"></textarea>
+                                </div>
+                            </div>
+                            <div class="row mb-3">
+                                <label class="col-sm-6 col-form-label">Genre(s)</label>
+                                <div class="row col-sm-6">
+                                    <div class="col-sm-4">
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="checkbox">
+                                            <label class="form-check-label" for="gridCheck1">
+                                                Example checkbox
+                                            </label>
+                                        </div>
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="checkbox">
+                                            <label class="form-check-label" for="gridCheck1">
+                                                Example checkbox
+                                            </label>
+                                        </div>
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="checkbox">
+                                            <label class="form-check-label" for="gridCheck1">
+                                                Example checkbox
+                                            </label>
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-4">
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="checkbox">
+                                            <label class="form-check-label" for="gridCheck1">
+                                                Example checkbox
+                                            </label>
+                                        </div>
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="checkbox">
+                                            <label class="form-check-label" for="gridCheck1">
+                                                Example checkbox
+                                            </label>
+                                        </div>
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="checkbox">
+                                            <label class="form-check-label" for="gridCheck1">
+                                                Example checkbox
+                                            </label>
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-4">
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="checkbox">
+                                            <label class="form-check-label" for="gridCheck1">
+                                                Example checkbox
+                                            </label>
+                                        </div>
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="checkbox">
+                                            <label class="form-check-label" for="gridCheck1">
+                                                Example checkbox
+                                            </label>
+                                        </div>
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="checkbox">
+                                            <label class="form-check-label" for="gridCheck1">
+                                                Example checkbox
+                                            </label>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row mb-3">
+                                <label class="col-sm-6 col-form-label">Price</label>
+                                <div class="col-sm-6">
+                                    <input type="text" class="form-control">
+                                </div>
+                            </div>
+                            <div class="row mb-3">
+                                <label class="col-sm-6 col-form-label">Cover</label>
+                                <div class="col-sm-6">
+                                    <input type="file" class="form-control">
+                                </div>
+                            </div>
+                            <button type="submit" class="btn btn-primary col-sm-4">Insert</button>
+                        </div>
+                    </div>
+                </form>
                 <table class="table">
                     <thead>
                     <tr>
@@ -80,41 +122,41 @@
                     </thead>
                     <tbody>
                     <tr>
-                        <th scope="row">Buku1</th>
+                        <td>Buku1</td>
                         <td>Mark</td>
                         <td>Otto</td>
                         <td>@mdo</td>
-                        <td>1000</td>
+                        <td>IDR 1000</td>
                         <td>
                             <div class="d-grid gap-2 d-md-block">
-                                <button class="btn btn-primary" type="button">Button</button>
-                                <button class="btn btn-primary" type="button">Button</button>
+                                <button class="btn btn-secondary" type="button">View Detail</button>
+                                <button class="btn btn-danger" type="button">Delete</button>
                             </div>
                         </td>
                     </tr>
                     <tr>
-                        <th scope="row">Buku2</th>
+                        <td>Buku2</td>
                         <td>Jacob</td>
                         <td>Thornton</td>
                         <td>@fat</td>
-                        <td>1000</td>
+                        <td>IDR 1000</td>
                         <td>
                             <div class="d-grid gap-2 d-md-block">
-                                <button class="btn btn-primary" type="button">Button</button>
-                                <button class="btn btn-primary" type="button">Button</button>
+                                <button class="btn btn-secondary" type="button">View Detail</button>
+                                <button class="btn btn-danger" type="button">Delete</button>
                             </div>
                         </td>
                     </tr>
                     <tr>
-                        <th scope="row">Buku3</th>
+                        <td>Buku3</td>
                         <td>Larry</td>
                         <td>Bird</td>
                         <td>@twitter</td>
-                        <td>1000</td>
+                        <td>IDR 1000</td>
                         <td>
                             <div class="d-grid gap-2 d-md-block">
-                                <button class="btn btn-primary" type="button">Button</button>
-                                <button class="btn btn-primary" type="button">Button</button>
+                                <button class="btn btn-secondary" type="button">View Detail</button>
+                                <button class="btn btn-danger" type="button">Delete</button>
                             </div>
                         </td>
                     </tr>
