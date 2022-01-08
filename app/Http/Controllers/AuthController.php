980efea6eaafb7341c0f3a3cd5227498dc5530cc
nehemiacecio <nehemia.cecio.3@gmail.com>
2022-01-08 13:54:56 +0700

    Auth Controller, Change Password#2

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use cApp\Models\User;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
