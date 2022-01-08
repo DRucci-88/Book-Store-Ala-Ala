6003d5012ee0156802b77d5ef10f1038c31a209f
Darren-34810 <darren.denisson@student.umn.ac.id>
2022-01-08 22:10:42 +0700

    Git testing

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -9,31 +9,35 @@ class AdminController extends Controller
 {
     public function manageBook()
     {
-        return view('admin.manage_book',[
+        return view('admin.manage_book', [
             'books' => Book::all()
         ]);
     }
 
     public function bookDetail(Book $book)
     {
-//        dd($book);
-        return view('admin.book_detail',[
+        //        dd($book);
+        return view('admin.book_detail', [
             'book' => $book
         ]);
     }
     public function updateBook(Book $book, Request $req)
     {
-//        dd($book);
-//        dd($req->input());
+        //        dd($book);
+        //        dd($req->input());
         $book->name = $req['name'];
         $book->author = $req['author'];
         $book->price = $req['price'];
         $book->synopsis = $req['synopsis'];
         $book->cover =  $req['cover'] ?? $req['oldCover'];
 
-        if($book->save()) {
+        if ($book->save()) {
             return back()->with('successMessage', 'Book Update Successfully');
         }
         return back()->with('errorMessage', 'Book Update Failed');
+    }
+    public function testgit()
+    {
+        echo "Testttttt";
     }
 }
