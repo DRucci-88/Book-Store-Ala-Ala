72983639773776574ba8528863e0a92e0a2909d7
DRucci-88 <60127414+DRucci-88@users.noreply.github.com>
2022-02-11 22:29:55 +0700

    CDN Jquery

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -43,6 +43,8 @@
         </div>
     </div>
     {{-- JQUERY --}}
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+        integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 
     <script>
         const x = document.getElementById('password');
